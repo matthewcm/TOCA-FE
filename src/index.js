6f2ab2a79e31be3013c9d9971656c89e4b783ff8
matthewcm <27matt37@gmail.com>
2020-12-29 18:48:09 +0000

    Redux Boilerplate

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import store from './app/store'
+import {Provider} from 'react-redux'
+
 import './styles/index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
   document.getElementById('root')
 );
 
